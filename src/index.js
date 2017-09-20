1436f77f751cb9647ccbb459d11e8734d1e96e77
zeven <fangfan1997@126.com>
2017-09-20 11:18:46 +0800

    use dva

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
+import React from 'react';
 import './config/ReactotronConfig';
+import dva from 'dva/mobile'
+import models from './models'
 import App from './App';
 
-export default App;
+const app = dva({
+  onError (e) {
+    console.warn(e)
+  }
+});
+models.map(m=>app.model(m));
+app.router(() => <App />);
+
+export default app.start();
